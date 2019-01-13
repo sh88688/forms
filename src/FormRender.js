d3986d3b1c04546f693b55c777aeda191829aff5
sh88688 <sh88688@gmail.com>
2019-01-14 02:03:32 +0530

    add json file and fix validation

--- src/FormRender.js
+++ src/FormRender.js
@@ -8,186 +8,46 @@ import CardContent from "@material-ui/core/CardContent";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import checkValidity from "./Validator";
-
+import jsonData from "./JsonData/formJson";
 class FormRender extends Component {
-  state = {
-    orderForm: {
-      name: {
-        elementType: "input",
-        elementConfig: {
-          type: "password",
-          label: "Emp Id",
-          placeholder: "Your Emp ID",
-          defaultValue: "",
-          helperText: "",
-          variant: "outlined",
-          margin: "normal"
-        },
-        value: "",
-        validation: {
-          required: true,
-          minLength: 5
-        },
-        valid: false,
-        touched: false
-      },
-      gender: {
-        elementType: "radio",
-        elementConfig: {
-          configs: {
-            name: "gender",
-            label: "Your Gender",
-            defaultValue: "male",
-            color: "secondary",
-            margin: "normal"
-          },
-          options: [
-            { value: "male", label: "Male", labelPlacement: "start" },
-            { value: "female", label: "Female", labelPlacement: "start" },
-            { value: "other", label: "Other", labelPlacement: "start" }
-          ]
-        },
-        value: "female",
-        validation: {},
-        valid: false,
-        touched: false
-      },
-      street: {
-        elementType: "input",
-        elementConfig: {
-          type: "text",
-          helperText: "",
-          label: "Street",
-          variant: "outlined",
-          margin: "normal"
-        },
-        value: "",
-        validation: {
-          required: true
-        },
-        valid: false,
-        touched: false
-      },
-      zipCode: {
-        elementType: "input",
-        elementConfig: {
-          type: "text",
-          label: "Postal Code",
-          helperText: "",
-          variant: "outlined",
-          margin: "normal"
-        },
-        value: "",
-        validation: {
-          required: true,
-          minLength: 5,
-          maxLength: 5
-        },
-        valid: false,
-        touched: false
-      },
-      country: {
-        elementType: "input",
-        elementConfig: {
-          type: "text",
-          label: "Country",
-          helperText: "",
-          variant: "outlined",
-          margin: "normal"
-        },
-        value: "",
-        validation: {
-          required: true,
-          minLength: 5
-        },
-        valid: false,
-        touched: false
-      },
-      email: {
-        elementType: "input",
-        elementConfig: {
-          type: "email",
-          label: "E-mail",
-          helperText: "",
-          variant: "outlined",
-          margin: "normal"
-        },
-        value: "",
-        validation: {
-          required: true
-        },
-        valid: false,
-        touched: false
-      },
-      dob: {
-        elementType: "input",
-        elementConfig: {
-          type: "date",
-          label: "Birthday",
-          helperText: "",
-          variant: "outlined",
-          margin: "normal",
-          InputLabelProps: { shrink: true }
-        },
-        value: "",
-        validation: {
-          required: true
-        },
-        valid: false,
-        touched: false
-      },
-      deliveryMethod: {
-        elementType: "select",
-        elementConfig: {
-          options: [
-            { value: "fastest", displayValue: "Fastest" },
-            { value: "cheapest", displayValue: "Cheapest" }
-          ],
-          configs: {
-            variant: "outlined",
-            margin: "normal",
-            label: "Delivery",
-            InputLabelProps: { shrink: true },
-            helperText: "Select Your Delivery Method"
-          }
-        },
-        value: "fastest",
-        validation: {},
-        valid: true
-      }
-    },
-    formIsValid: false,
-    loading: false
-  };
+  //constructor
+  constructor(props){
+
+    super(props);
+   
+    this.state = {
+      sampleForm: jsonData,
+      formIsValid: false,
+      loading: false
+    };
+    
+  }
+
 
   orderHandler = event => {
     event.preventDefault();
     this.setState({ loading: true });
     const formData = {};
-    for (let formElementIdentifier in this.state.orderForm) {
-      formData[formElementIdentifier] = this.state.orderForm[
-        formElementIdentifier
-      ].value;
+    for (let formElementIdentifier in this.state.sampleForm) {
+      formData[formElementIdentifier] = this.state.sampleForm[formElementIdentifier].value;
     }
-    const order = {
-      ingredients: this.props.ingredients,
-      price: this.props.price,
-      orderData: formData
-    };
-    console.log(order);
+    
+    alert(JSON.stringify(formData));
+   
   };
 
   inputChangedHandler = (event, inputIdentifier) => {
-    //make a copy of orderForm State
-    const updatedOrderForm = {
-      ...this.state.orderForm
+    //make a copy of sampleForm State
+    const updatedsampleForm = {
+      ...this.state.sampleForm
     };
     // make a copy of Changed Element
     const updatedFormElement = {
-      ...updatedOrderForm[inputIdentifier]
+      ...updatedsampleForm[inputIdentifier]
     };
     //update changed value
     updatedFormElement.value = event.target.value;
+    
     //check validity
     let getValidity = checkValidity(
       updatedFormElement.value,
@@ -198,28 +58,28 @@ class FormRender extends Component {
     updatedFormElement.elementConfig.helperText = getValidity.errorText;
     //updated element's touched property
     updatedFormElement.touched = true;
-    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    updatedsampleForm[inputIdentifier] = updatedFormElement;
 
     //Checking The whole form Validity
     let formIsValid = true;
-    for (let inputIdentifier in updatedOrderForm) {
-      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    for (let inputIdentifier in updatedsampleForm) {
+      formIsValid = updatedsampleForm[inputIdentifier].valid && formIsValid;
     }
-    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
-    //console.log(this.state.orderForm[inputIdentifier]);
+    this.setState({ sampleForm: updatedsampleForm, formIsValid: formIsValid });
+    //console.log(this.state.sampleForm[inputIdentifier]);
   };
 
   render() {
     const formElementsArray = [];
 
-    for (let key in this.state.orderForm) {
+    for (let key in this.state.sampleForm) {
       formElementsArray.push({
         id: key,
-        config: this.state.orderForm[key]
+        config: this.state.sampleForm[key]
       });
     }
     let form = (
-      <form onSubmit={this.orderHandler}>
+      <form >
         {formElementsArray.map(formElement => (
           <InputBuilder
             key={formElement.id}
@@ -233,8 +93,8 @@ class FormRender extends Component {
             changed={event => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
-        <Button variant="contained" color="primary">
-          Order
+        <Button variant="contained" onClick={this.orderHandler} disabled={!this.state.formIsValid} color="primary">
+          Send Form
         </Button>
       </form>
     );
@@ -249,7 +109,7 @@ class FormRender extends Component {
           </Toolbar>
         </AppBar>
         <br />
-        {JSON.stringify(this.state.formIsValid)}
+       
         <br />
         <Grid container spacing={24} justify={"center"}>
           <Grid item xs={6}>
--- src/Validator.js
+++ src/Validator.js
@@ -1,27 +1,49 @@
 function checkValidity(value, rules) {
     let validityObj = { isValid: true, errorText: "" };
+    
     if (rules.required) {
       validityObj.isValid = value.trim() !== "" && validityObj.isValid;
       validityObj.errorText =
         value.trim() !== "" ? validityObj.errorText : "* This field is required.";
     }
   
-    if (rules.minLength) {
+    if (rules.minLength && value.trim() !== "") {
       validityObj.isValid =
         value.length >= rules.minLength && validityObj.isValid;
       validityObj.errorText =
         value.length >= rules.minLength
           ? validityObj.errorText
-          : "* Min Length is 5.";
+          : `* Min Length is  ${rules.minLength}.`;
     }
   
-    if (rules.maxLength) {
+    if (rules.maxLength && value.trim() !== "") {
       validityObj.isValid =
         value.length <= rules.maxLength && validityObj.isValid;
       validityObj.errorText =
         value.length <= rules.maxLength
           ? validityObj.errorText
-          : "* Max Length is 5.";
+          : `* Max Length is ${rules.maxLength}.`;
+    }
+  
+    if (rules.isEmail && value.trim() !== "") {
+      let regex = /^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,3})$/;
+
+      validityObj.isValid =
+        regex.test(value) && validityObj.isValid;
+      validityObj.errorText =
+        regex.test(value)
+          ? validityObj.errorText
+          : "Please enter a valid e-mail.";
+    }
+    if (rules.isDigit && value.trim() !== "") {
+      let regex = /^\d+$/;
+
+      validityObj.isValid =
+        regex.test(value) && validityObj.isValid;
+      validityObj.errorText =
+        regex.test(value)
+          ? validityObj.errorText
+          : "Please enter only digits.";
     }
   
     return validityObj;
