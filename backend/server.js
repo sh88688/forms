1938f4504fc63ac3e41b6c97a6c56500b8e8931b
sh88688 <sh88688@gmail.com>
2019-01-18 00:39:49 +0530

    adding backend and spliting project into modules

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+
+const express = require('express')
+const app = express()
+const port = 3001
+var form =  require("./JsonData/formJson.json");
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
+app.get('/', (req, res) => res.send(form))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
